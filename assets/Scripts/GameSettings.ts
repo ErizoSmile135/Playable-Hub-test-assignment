6031089b1176ea87c44abf870f40b2d025c260fa
Maksim <maxyarmoluk@gmail.com>
2025-04-30 15:51:29 +0300

    Адаптация под разрешение экрана

--- assets/Scripts/GameSettings.ts
+++ assets/Scripts/GameSettings.ts
@@ -13,16 +13,19 @@ export default class Canvas extends cc.Component {
         const myWidth = 720;
         const myHeight = 1280;
 
-        const scaleFactor = width / myWidth;
+        /*const scaleFactor = width / myWidth;
         const scale = Math.min(Math.max(scaleFactor, 1.0), 2.0);
-        this.node.scale = scale;
+        this.node.scale = scale;*/
 
-        if (width / height > myWidth / myHeight) {    //если шире 
+        this.canvas.fitWidth = true;
+        this.canvas.fitHeight = true;
+
+        /*if (width / height > myWidth / myHeight) {    //если шире 
             this.canvas.fitWidth = true;
             this.canvas.fitHeight = false;
         } else {                              //если выше
             this.canvas.fitWidth = false;
             this.canvas.fitHeight = true;
-        }
+        }*/
     }
 }
